b34965da0e4efb55e31db0c66351131cb00b8d34
eeemons <eeemons@live.com>
2025-08-09 23:46:00 +0600

    needs working on the overlapping of the contents

--- Component/PhoneMockup.tsx
+++ Component/PhoneMockup.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import image from "@/images/phone-mockup.png";
 export default function PhoneMockup() {
   return (
-    <div className="relative max-w-[390px]">
+    <div className="relative max-w-[390px] md:max-w-[666px]">
       <Image
         src={image}
         alt="Phone mockup"
-        width={500}
+        width={666}
         height={500}
         priority
         className="block h-auto"
